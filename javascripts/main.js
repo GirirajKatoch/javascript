2559e013b1306348eb483e38b20aef67114198f0
gskatoch <giriraj.katoch@gmail.com>
2013-06-16 23:03:49 +0530

    Project Euler Problem 1
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
    Find the sum of all the multiples of 3 or 5 below 1000.
    Answer:233168
    [Completed on Sun, 16 Jun 2013, 14:02]
    
    This problem was solved using JavaScript.
    Three new JavaScript in-built/Library functions I learn from solving this problem are as follows:
    1. push() - appends (push) elements into an array. Used to populate array using a for loop. This array became the data set to be processed.
    2. filter() - filters the elements from an existing array (in this case the array populated by push as mentioned above) based on a filter condition specified by taking in a function as a parameter. In this case , the function to filter array elements based on the condition that element must be multiples of 3 or 5.
    3. reduce() - To sum all the elements in the final filtered array to give the final sum.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+range = function(n) {
+  results = [];
+  for(i=0;i<=n;i++) {
+      results.push(i);
+  }
+  return results;
+};
+console.log(range(999).filter(function(n){return (n%3===0 || n%5===0)}).reduce(function(a,b){return a+b}));
